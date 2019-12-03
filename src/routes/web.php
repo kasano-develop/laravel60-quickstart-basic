7a7e9e900a2db7a2007bdeafdb5d5e88606070c7
Keisuke Kasano <keisuke.kasano@smaregi.jp>
2019-12-03 17:56:22 +0900

    - sample task tool (https://qiita.com/ucan-lab/items/36f6e89abad26a68f69a)

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,6 +11,47 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
+/**
+ * タスクダッシュボード表示
+ */
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+/**
+ * 新タスク追加
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // タスク作成処理…
+    $task = new Task();
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * タスク削除
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect('/');
 });
